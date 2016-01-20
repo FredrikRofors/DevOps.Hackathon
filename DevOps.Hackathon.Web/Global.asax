53e31e269537b2c34857194ede617e9955ace85c
Fredrik Rofors <fredrik_rofors@hotmail.com>
2016-01-20 13:17:53 +0100

    Out-of-the-box ASP.NET MVC project with unit tests

---
+++ DevOps.Hackathon.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevOps.Hackathon.Web.MvcApplication" Language="C#" %>
---
+++ DevOps.Hackathon.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOps.Hackathon.Web.Startup))]
+namespace DevOps.Hackathon.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
